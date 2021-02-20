11d5136fcf1ef537428cfde5d976d0916455b280
Amalawa <aiweka@uw.edu>
2021-02-19 19:44:43 -0800

    I fixed the file path

---
+++ project_data/explanatory_analysis/chart_1.R
@@ -0,0 +1,31 @@
+setwd("~/INFO_201/project-ishupdl/project_data/data_charts")
+social_media <- read.csv(file = 'whatsgoodlydata_6.csv')
+
+library(dplyr)
+install.packages("ggplot2")
+install.packages(reshape2)
+install.packages(lintr)
+library(ggplot2)
+
+#I want to compare which social media app influences 
+#shopping the most among female respondents 
+
+female_response <- social_media %>% 
+  filter(segment_description == "Female voters") %>% 
+  select(count, answer)
+
+
+answers <- female_response$answer
+count <- female_response$count
+
+df_x <- data.frame(count, answers)
+df_x
+
+chart_x <- ggplot(df_x, aes(x=answers, y=count, group=answers)) + 
+  geom_bar(stat = "identity")+ 
+  scale_fill_manual(values=c("#55DDE0", "#33658A", "#2F4858", "#F6AE2D", "#F26419", "#999999")) + 
+  labs(title = "App Votes from Female Responders")
+chart_x 
+
+
+
